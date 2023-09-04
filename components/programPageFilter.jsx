15d5551cd8165fe4b03d1181d035d0569df6f90b
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-09-04 16:01:58 +0900

    add program filter

---
+++ components/programPageFilter.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useCnStore } from "@/store/store";
+import { useEffect } from "react";
+
+export default function ProgramPageFilter() {
+  
+
+  return (
+    
+    <ul className="grid grid-cols-6 gap-1">
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+    </ul>
+  );
+}
